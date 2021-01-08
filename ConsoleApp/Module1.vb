824023e7ec6a167f44e326a083c7b31a850f5d28
lm250326 <lm250326@ncr.com>
2021-01-08 12:27:55 +0530

    Added utility class.

--- ConsoleApp/Module1.vb
+++ ConsoleApp/Module1.vb
@@ -2,6 +2,10 @@
 
     Sub Main()
         Console.WriteLine("Hello Cardnox")
+
+        Dim ut As New Utility()
+        Console.WriteLine(ut.SumOfTwoNumbers(10, 20))
+
         Console.ReadLine()
 
     End Sub
---
+++ ConsoleApp/Utility.vb
@@ -0,0 +1,9 @@
+﻿Public Class Utility
+
+    Function SumOfTwoNumbers(a As Integer, b As Integer) As Integer
+
+        Return a + b
+    End Function
+
+
+End Class
