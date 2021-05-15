5d85b949fc3411584c4517e7d28ff9a0d4736c54
Lakshmi Narayana Malempati <Lakshmi.malempati@thoughtfocus.com>
2021-05-15 15:33:04 +0530

    - moved all utility methods to VBPractice module

--- VBPractice/Utility.vb
+++ VBPractice/Utility.vb
@@ -63,4 +63,35 @@ Public Module Utility
             Return n
         End If
     End Function
+
+    Function SumOfTwoNumbers(a As Integer, b As Integer) As Integer
+
+        Return a + b
+    End Function
+
+    'Find max of given numbers
+    Function Findmax(first As Integer, second As Integer) As Integer
+        If first > second Then
+            Return first
+        Else
+            Return second
+        End If
+    End Function
+
+    Function FindMaxOfGivenNumbers(Nums() As Integer) As Integer
+        Dim max As Integer
+        max = Nums(0)
+
+        For Each item As String In Nums
+            If item > max Then
+                max = item
+            End If
+        Next
+        Return max
+    End Function
+
+    Function isHex(value As String)
+        'Return xCore.IsHex(value)
+        Return RegularExpressions.Regex.IsMatch(value, "\A\b[0-9a-fA-F]+\b\Z")
+    End Function
 End Module
